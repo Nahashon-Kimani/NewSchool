c5436304d26c8d8f76f957df8e834c1c57c2b3f3
Nahashon Kimani <nahashonmbuci@gmail.com>
2018-09-27 21:22:45 +0300

    Expandable Layout

--- app/src/main/java/com/nahashon/school/AskTeacher.java
+++ app/src/main/java/com/nahashon/school/AskTeacher.java
@@ -2,12 +2,20 @@ package com.nahashon.school;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class AskTeacher extends AppCompatActivity {
-
+     RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ask_teacher);
+
+        recyclerView = findViewById(R.id.askTeacher);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(new AskTeacherRecyclerAdapter(this));
     }
 }
---
+++ app/src/main/java/com/nahashon/school/AskTeacherRecyclerAdapter.java
@@ -0,0 +1,57 @@
+package com.nahashon.school;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import net.cachapa.expandablelayout.ExpandableLayout;
+
+public class AskTeacherRecyclerAdapter extends RecyclerView.Adapter<AskTeacherRecyclerAdapter.MyHolder> {
+
+    Context context;
+    public AskTeacherRecyclerAdapter(Context context){
+        this.context = context;
+
+
+    }
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.ask_a_teacher_strip,viewGroup,false);
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MyHolder myHolder, int i) {
+         myHolder.l.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 myHolder.el.toggle(true);
+                 myHolder.el.toggle();
+             }
+         });
+    }
+
+    @Override
+    public int getItemCount() {
+        return 20;
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder{
+         ExpandableLayout el;
+         LinearLayout l;
+        public MyHolder(View view){
+            super(view);
+            el = view.findViewById(R.id.expandableListView);
+            l = view.findViewById(R.id.expandable_strip);
+
+
+        }
+
+    }
+}
