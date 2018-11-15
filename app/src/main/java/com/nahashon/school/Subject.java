860eda731b22639a9a4c436ffbcce44e296c9e58
karusamuel <karusamuelngugi@gmail.com>
2018-11-15 21:55:35 +0300

    my name

--- app/src/main/java/com/nahashon/school/Subject.java
+++ app/src/main/java/com/nahashon/school/Subject.java
@@ -5,7 +5,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Subject extends AppCompatActivity {
